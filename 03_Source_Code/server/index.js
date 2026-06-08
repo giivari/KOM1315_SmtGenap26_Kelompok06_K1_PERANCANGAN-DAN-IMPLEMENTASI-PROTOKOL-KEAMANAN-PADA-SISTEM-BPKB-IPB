138424ab621c19b839505ea3e6b15fa6b28a09f5
givari <givari.zacky@gmail.com>
2026-06-08 19:54:04 +0700

    debug railway port

--- 03_Source_Code/server/index.js
+++ 03_Source_Code/server/index.js
@@ -68,6 +68,7 @@ app.use(helmet({
  */
 const allowedOrigins = [
   process.env.CLIENT_URL || 'http://localhost:5174',
+  'https://bpkb-ipb.vercel.app',
   'http://localhost:5173',
   'http://localhost:3000',
 ];
